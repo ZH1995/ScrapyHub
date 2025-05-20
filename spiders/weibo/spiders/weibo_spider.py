b4c3b503d874e4e9e7f8090c7579b9b045d4ffa5
zh1995 <rwzsjfm@qq.com>
2025-05-20 18:20:35 +0800

    weibo抓取继承自公共基础库

--- spiders/weibo/spiders/weibo_spider.py
+++ spiders/weibo/spiders/weibo_spider.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import scrapy
 import urllib.parse
 import json
+from ..items import WeiboItem
 
 class WeiboSpider(scrapy.Spider):
     name = 'weibo'
@@ -10,14 +11,12 @@ class WeiboSpider(scrapy.Spider):
 
     def start_requests(self):
         for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse, headers={
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-            })
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         try:
             data = json.loads(response.text)
-            print("成功解析JSON数据")
+            self.logger.info("成功解析JSON数据")
             if 'realtime' in data['data']:
                 hot_searches = data['data']['realtime']
                 for item in hot_searches:
@@ -27,12 +26,12 @@ class WeiboSpider(scrapy.Spider):
                     query = '#' + title + '#' if topic_flag else title
                     encoded_query = urllib.parse.quote(query)
                     url = f"https://s.weibo.com/weibo?q={encoded_query}&t=31"
+                    
                     if title and url:
-                        yield {
-                            'title': title,
-                            'url': url,
-                            'hot_rank': hot_rank,
-                        }
+                        yield WeiboItem(
+                            title=title,
+                            url=url,
+                            hot_rank=hot_rank
+                        )
         except json.JSONDecodeError as e:
-            print("无法解析JSON数据")
-            print(response.text)
+            self.logger.error(f"无法解析JSON数据: {e}")
